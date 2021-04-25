1732d0723a65455657f78d9d65f1e42760292c66
Alexander Loginov <lxndrloginov@gmail.com>
2021-04-25 18:49:56 +0300

    added AND to logical_operations

--- logical_operations.py
+++ logical_operations.py
@@ -6,6 +6,8 @@ class LogicalOperations():
         self.neuron = bn.Neuron()
 
     def not_func(self, x=1):
+        self.neuron.clear_all()
+
         w = -2
         b = 1
         self.neuron.add_x(x)
@@ -15,10 +17,21 @@ class LogicalOperations():
 
         self.neuron.step_func()
 
+    def and_func(self, x1=1, x2=1):
+        self.neuron.clear_all()
 
-def test():
-    not_operation = LogicalOperations()
-    not_operation.not_func(1)
+        w1 = 2
+        w2 = 2
+        b = -3
+        self.neuron.add_x(x1, x2)
+        self.neuron.add_weights(w1, w2)
+
+        self.neuron.summator(b)
+
+        self.neuron.step_func()
 
 
-# print(test())
+operation = LogicalOperations()
+
+operation.not_func(1)
+operation.and_func(1, 1)

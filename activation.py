85a90d7d5048650c30b019154ba9edf81b8425c7
Alexander Loginov <lxndrloginov@gmail.com>
2021-12-06 23:58:33 +0300

    Added Sigmoid and Step activation funcs

--- activation.py
+++ activation.py
@@ -4,3 +4,15 @@ import numpy as np
 class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
+
+
+class Activation_Sigmoid:
+    def forward(self, inputs):
+        self.output = 1 / (1 + np.exp(-inputs))
+
+
+class Activation_Step:
+    # сначала округляем в большую сторону и спасаем все значения X > 0
+    # потом все значения приравниваем к единице
+    def forward(self, inputs):
+        self.output = np.minimum(1, np.maximum(0, np.ceil(inputs)))
